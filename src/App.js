ca42ba7bac938c9566595c43a209f9491dd4cfda
Jiahua Cui <jiahuacui@Jiahuas-iMac.home>
2021-06-23 17:13:11 -0400

    commit before branch change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
           <NavBar />
+
       </div>
     </BrowserRouter>
   );
